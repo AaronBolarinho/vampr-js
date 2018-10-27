228e0d24a3b8ca5a39c4332088a8b0a077d67794
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-27 00:28:15 +0000

    done

--- vampire.js
+++ vampire.js
@@ -10,22 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVamps++;
+    }
+
+    return numberOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if(this.numberOfVampiresFromOriginal === 0) {
+      return true;
+    } else if(vampire.numberOfVampiresFromOriginal === 0) {
+      return false;
+    } else if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else if (this.numberOfVampiresFromOriginal >= vampire.numberOfVampiresFromOriginal) {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -35,10 +52,29 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  // closestCommonAncestor(vampire) {
 
-  }
+  // }
 }
 
+//Original vampire
+const original = new Vampire("Original", 800);
+
+// Bart
+const bart = new Vampire("Bart", 800);
+
+// Ansel
+const ansel = new Vampire("Ansel", 800);
+
+// Elgort
+const elgort = new Vampire("Elgort", 800);
+
+// Sarah
+const sarah = new Vampire("Sarah", 800);
+
+// Andrew
+const andrew = new Vampire("Andrew", 800);
+
+//------
 module.exports = Vampire;
 
